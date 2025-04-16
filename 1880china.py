a4b532ebe445b56cd61683eb4baefaf5817b58f1
Linus Callheim <stoltefar@gmail.com>
2025-04-16 19:20:03 +0200

    Add MajorCompany Class with launch method. Added three test companies.

--- 1880china.py
+++ 1880china.py
@@ -8,6 +8,7 @@ print("Number of players: " + (str(PLAYER_COUNT)))
 # TODO Let user add Players in any order. 
 # TODO Create setup share round method to handle auctioning of Private Companies and the resulting player expenses and stock gains.
 # TODO Sort player order after remaining cash.
+
 class Player:
     """A container for keeping track of the holdings for each player."""
 
@@ -15,6 +16,30 @@ class Player:
     def __init__(self, name, wallet):
         self.name = name
         self.wallet = wallet
+class MajorCompany:
+    """A container for keeping track of a Major company's stock value, share certificates and share holders (=Players).
+    May also contain data for company funds and trains beyond MVP product."""
+
+    def __init__(self, name, abreviation):
+        self.name = name
+        self.abreviation = abreviation
+        self.shares = 10
+        self.directorShares = 2
+        
+    def launch(self, director, parValue, directorShares):
+        self.floated = False
+        self.parValue = 0
+        self.director = Player()
+        self.stockBook = []
+
+# Initiate Major companies
+
+bcr = MajorCompany("Baocheng Railway", "BCR")
+jha = MajorCompany("Jingha Railway", "JHA")
+hkr = MajorCompany("Hukun Railway", "HKR")     
+
+
+
 
 print("\nPerform setup Share round by auctioning out the Private Companies P0 - P7.")
 
