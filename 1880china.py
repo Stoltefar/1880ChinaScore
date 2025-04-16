9e3a35cb625a022efb1afdea21f30a4fb864c8f9
Linus Callheim <stoltefar@gmail.com>
2025-04-16 08:44:37 +0200

    Add constant for Player count and TODOs

--- 1880china.py
+++ 1880china.py
@@ -1,2 +1,13 @@
 # Main app that will create a game asking how many players are in it and create objects of Player, MajorCompanies classes.
 
+print("How many players are in this game?")
+PLAYER_COUNT = int(input())
+
+print("Number of players: " + (str(PLAYER_COUNT)))
+
+# TODO Let user add Players in any order. 
+# TODO Create setup share round method to handle auctioning of Private Companies and the resulting player expenses and stock gains.
+# TODO Sort player order after remaining cash.
+
+print("Perform setup Share round by auctioning out the Private Companies P0 - P7.")
+print("In order of remaining cash, starting from the bottom, enter Player info.")
